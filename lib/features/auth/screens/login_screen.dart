9c8892d8131ab24e9559f13f0ba1c19d8b91f9fd
Mohammed-Fahiz <mohammedfahiz73@gmail.com>
2023-12-18 11:32:13 +0530

    login functionality

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:gramboo_sales_details/core/navigation_services.dart';
 import 'package:gramboo_sales_details/core/utilities/custom_dropDown.dart';
 import 'package:gramboo_sales_details/features/dashboard/screens/dashBoard_screen.dart';
@@ -21,6 +22,8 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
 
   final obscureTextProvider = StateProvider((ref) => true);
 
+  final isLoginSuccessProvider = StateProvider((ref) => false);
+
   final branchValueProvider = StateProvider<String?>((ref) => "branch 1");
 
   @override
@@ -32,115 +35,208 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final isLoggedIn = ref.watch(isLoginSuccessProvider);
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _userNameController,
-                decoration: const InputDecoration(
-                  hintText: "Enter username",
-                  prefixIcon: Icon(
-                    Icons.account_circle_sharp,
-                    color: Palette.iconColor,
-                  ),
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 3, color: Colors.redAccent),
-                  ),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Palette.primaryColor,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Palette.primaryColor,
-                    ),
+      body: Center(
+        child: Container(
+          height: isLoggedIn ? h * .6 : h * .5,
+          padding: const EdgeInsets.all(20),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Login",
+                        style: GoogleFonts.alice(fontSize: w * .1),
+                      ),
+                      SizedBox(
+                        height: h * .02,
+                      ),
+                      TextFormField(
+                        controller: _userNameController,
+                        decoration: InputDecoration(
+                          hintText: "Enter username",
+                          prefixIcon: const Icon(
+                            Icons.account_circle_sharp,
+                            color: Palette.iconColor,
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.redAccent),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: const BorderSide(
+                              color: Palette.primaryColor,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: const BorderSide(
+                              color: Palette.primaryColor,
+                            ),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.trim().isEmpty) {
+                            return "This field can't be empty";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: h * .01,
+                      ),
+                      TextFormField(
+                        obscureText: ref.watch(obscureTextProvider),
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          hintText: "Enter password",
+                          prefixIcon: const Icon(
+                            Icons.lock,
+                            color: Palette.iconColor,
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: const BorderSide(
+                                width: 3, color: Colors.redAccent),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: const BorderSide(
+                              color: Palette.borderColor,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: const BorderSide(
+                              color: Palette.borderColor,
+                            ),
+                          ),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              if (ref.read(obscureTextProvider)) {
+                                ref
+                                    .read(obscureTextProvider.notifier)
+                                    .update((state) => false);
+                              } else {
+                                ref
+                                    .read(obscureTextProvider.notifier)
+                                    .update((state) => true);
+                              }
+                            },
+                            icon: const Icon(
+                              Icons.remove_red_eye_outlined,
+                              color: Palette.iconColor,
+                            ),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Enter password";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: h * .02,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          if (!isLoggedIn) {
+                            login();
+                          }
+                        },
+                        child: Container(
+                          height: h * .06,
+                          width: w * .4,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Palette.primaryColor),
+                          child: Center(
+                            child: Text(
+                              isLoggedIn ? "Logged in" : "Log in",
+                              style: TextStyle(
+                                fontSize: w * .06,
+                                color: Palette.textColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      // if login success branch select drop down
+                      if (isLoggedIn)
+                        Column(
+                          children: [
+                            SizedBox(
+                              height: h * .02,
+                            ),
+                            Text(
+                              "Select branch",
+                              style: GoogleFonts.alice(fontSize: w * .05),
+                            ),
+                            SizedBox(
+                              height: h * .02,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                CustomDropDown(
+                                  dropList: const [
+                                    "branch 1",
+                                    "branch 2",
+                                    "branch 3",
+                                  ],
+                                  selectedValueProvider: branchValueProvider,
+                                ),
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Container(
+                                    height: h * .06,
+                                    width: w * .4,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(15),
+                                        color: Palette.primaryColor),
+                                    child: Center(
+                                      child: Text(
+                                        "Continue",
+                                        style: TextStyle(
+                                          fontSize: w * .06,
+                                          color: Palette.textColor,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )
+                    ],
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                obscureText: ref.watch(obscureTextProvider),
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  hintText: "Enter password",
-                  prefixIcon: const Icon(
-                    Icons.lock,
-                    color: Palette.iconColor,
-                  ),
-                  errorBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(width: 3, color: Colors.redAccent),
-                  ),
-                  border: const OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Palette.borderColor,
-                    ),
-                  ),
-                  focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Palette.borderColor,
-                    ),
-                  ),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      if (ref.read(obscureTextProvider)) {
-                        ref
-                            .read(obscureTextProvider.notifier)
-                            .update((state) => false);
-                      } else {
-                        ref
-                            .read(obscureTextProvider.notifier)
-                            .update((state) => true);
-                      }
-                    },
-                    icon: const Icon(
-                      Icons.remove_red_eye_outlined,
-                      color: Palette.iconColor,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: h * .05,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  CustomDropDown(dropList: [
-                    "branch 1",
-                    "branch 2",
-                    "branch 3",
-                  ], selectedValueProvider: branchValueProvider),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Palette.cardColor,
-                      fixedSize: const Size(150, 60),
-                    ),
-                    onPressed: () {
-                      NavigationService.navigateToScreen(
-                        context,
-                        DashBoardScreen(),
-                      );
-                    },
-                    child: const Text(
-                      "Log in",
-                      style: TextStyle(fontSize: 20, color: Palette.textColor),
-                    ),
-                  ),
-                ],
-              )
-            ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  login() {
+    // if login successful
+    if (_formKey.currentState!.validate()) {
+      ref.read(isLoginSuccessProvider.notifier).state = true;
+    }
   }
 }
