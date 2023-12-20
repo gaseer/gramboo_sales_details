06163957ed117d1d36466d90ecf6150c8803f233
gaseer <gaseermohammed8@gmail.com>
2023-12-20 17:39:52 +0530

    changed snackbar style

--- lib/core/utilities/custom_snackBar.dart
+++ lib/core/utilities/custom_snackBar.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-showSnackBar({required String content, required BuildContext context}) {
-  final snackBar = SnackBar(content: Text(content));
+showSnackBar(
+    {required String content, required BuildContext context, Color? color}) {
+  final snackBar = SnackBar(
+    content: Text(
+      content,
+      textAlign: TextAlign.center,
+      style: GoogleFonts.abel(
+          letterSpacing: 2, fontWeight: FontWeight.bold, fontSize: 20),
+    ),
+    backgroundColor: color,
+  );
   ScaffoldMessenger.of(context)
     ..hideCurrentSnackBar()
     ..showSnackBar(snackBar);
--- lib/features/auth/controller/loginController.dart
+++ lib/features/auth/controller/loginController.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gramboo_sales_details/core/utilities/custom_snackBar.dart';
 import 'package:gramboo_sales_details/features/auth/services/loginServices.dart';
@@ -27,13 +28,15 @@ class AuthController extends Notifier<bool> {
         .login(userName: userName, password: password);
 
     res.fold(
-      (l) => showSnackBar(content: l.errMSg, context: context),
+      (l) =>
+          showSnackBar(content: l.errMSg, context: context, color: Colors.red),
       (r) {
         NavigationService.navigateToScreen(
           context,
           const DashBoardScreen(),
         );
-        return showSnackBar(content: "Login success", context: context);
+        return showSnackBar(
+            content: "Login success", context: context, color: Colors.green);
       },
     );
   }
